25c0bf9f4d9d5d23ec58855fe8b62587561dbc0c
Sreya-C <98732604+Sreya-C@users.noreply.github.com>
2024-02-29 10:01:24 +0530

    Update views.py

--- IT/Week6/Sessions/navigation/views.py
+++ IT/Week6/Sessions/navigation/views.py
@@ -15,14 +15,9 @@ def firstpage(request):
     return render(request, 'firstpage.html', {'form': form})
         
 def secondpage(request):
-    name = request.GET.get('name')
-    roll = request.GET.get('roll')
-    subjects = request.GET.get('subjects')
+    name = request.session.get('name')
+    roll = request.session.get('roll')
+    subjects = request.session.get('subjects')
     if not name or not roll or not subjects:
         return HttpResponse("Data not found in session")
     return render(request, 'secondpage.html', {'name': name, 'roll': roll, 'subjects': subjects})
-    
-
-
-    
-            
